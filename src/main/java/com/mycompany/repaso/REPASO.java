0a92aad487eb35c840f31cf2045f292ef822bca2
lorena_olay <olaylorena2002@gmail.com>
2021-11-09 14:30:31 +0100

    hecho el try catch

--- src/main/java/com/mycompany/repaso/REPASO.java
+++ src/main/java/com/mycompany/repaso/REPASO.java
@@ -4,6 +4,9 @@
  */
 package com.mycompany.repaso;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author lorena
@@ -15,6 +18,35 @@ public class REPASO {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        Scanner sc = new Scanner(System.in);
+        int operador1, operador2;
+        boolean solicitarDatos = true;
+        /*
+        - MOSTRAMOS MENÚ OPCIONES
+        - FILTRAMOS LAS ENTRADAS DE LOS USUARIOS
+        - CONTROLAR LAS EXCEPCIONES POR SI EL USUARIO SE EQUIVOCA
+        - CUATRO OPCIONES: SUMAR, RESTAR, MULTIPLICAR Y DIVIDIR
+        
+         */
+        
+        System.out.println("Bienvenidos al calculato:");
+        System.out.println("Introduce dos operandos (entre 100 y -100).");
+        do{
+        try{
+            //Bloque de código candidaato a lanzar la excepción
+        System.out.println("Introduce operando 1: ");
+        operador1 = sc.nextInt();
+        System.out.println("Introduce operando 2: ");
+        operador2 = sc.nextInt();
+        solicitarDatos = false;
+        } catch(InputMismatchException ime){
+            //Código para tratar el error
+            System.out.println("Se ha introducido texto en lugar de números." + 
+                    "Vuelve a introducir los ");
+            sc.nextLine();
+        }
+        } while (solicitarDatos);
     }
-    
+
 }
